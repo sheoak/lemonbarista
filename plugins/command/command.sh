51685f3b4460f86c01ddfcd503706d6a82cda89c
Yannick Huerre <dev@sheoak.fr>
2015-11-22 22:37:15 +0100

    feat: Add basic command plugin

---
+++ plugins/command/command.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+# TODO: helper?
+optspec=":-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            case "${OPTARG}" in
+                name=*)
+                    name=${OPTARG#*=}
+                    ;;
+                command=*)
+                    cmd=${OPTARG#*=}
+                    ;;
+            esac;;
+    esac
+done
+
+if [ -z "$cmd" ]; then
+    echo "No command specified"
+    exit 1
+fi
+
+if [ -z "$name" ]; then
+    echo "No name specified"
+    exit 1
+fi
+
+res=`eval $cmd`
+
+echo "$name:"
+echo "    text: $res"
