cb7bd1d9a59a9ee30a4ba2384e9a50535addd5a1
Damien Nicolas <damien@gordon.re>
2015-11-23 19:32:16 +0100

    feat: Guess config type from extension in main script

--- src/lemonbarista.py
+++ src/lemonbarista.py
@@ -21,22 +21,29 @@ if __name__ == '__main__':
                         default=os.path.join(CONF_DIR, 'config.yaml'),
                         help='Loads the given configuration file')
     parser.add_argument('-t', '--type', action='store', dest='config_type',
-                        default='yaml', choices=('yaml', 'json'),
+                        default=None, choices=('yaml', 'json'),
                         required=False, help='Sets the config type (yaml or '
                         'json)')
 
     arguments = parser.parse_args()
 
     try:
-        with open(arguments.config_file, 'rb') as fd:
-            if arguments.config_type == 'yaml':
+        with open(arguments.config_file, 'r') as fd:
+            if arguments.config_type is not None:
+                config_type = arguments.config_type
+            else:
+                config_type = arguments.config_file.split('.')[-1].lower()
+
+            if config_type == 'yaml':
                 import yaml
                 config = yaml.load(fd)
                 print('Loading YAML')
-            elif arguments.config_type == 'json':
+            elif config_type == 'json':
                 import json
                 config = json.load(fd)
                 print('Loading JSON')
+            else:
+                raise ValueError('Type {} unknown'.format(config_type))
     except Exception as e:
         print(e)
         sys.exit(1)
